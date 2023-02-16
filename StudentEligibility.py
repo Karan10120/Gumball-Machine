20e5b00344994322aa0c8f0476734e0c7f0a74da
EtienneDevictor <84875282+EtienneDevictor@users.noreply.github.com>
2023-02-15 18:48:15 -0800

    Update StudentEligibility.py

--- StudentEligibility.py
+++ StudentEligibility.py
@@ -1,3 +1,10 @@
+from enum import Enum
+
+class Scholarship_Status(Enum):
+    INELIGABLE = 1
+    ELIGABLE = 2
+    DEAN_CONSIDERATION = 3
+
 # Takes infromation about the student and determines if they are eligable to recieve the scholarship 
 # @param age - age of the applicant, lived - the amount of time the applicant has lived in california
 # @param worked - amount of time the applicant has worked in CA, volunteered - true if the applicant has completed volunteer work false otherwise
@@ -16,7 +23,7 @@ def scholarship_eligibility(age, lived, worked, parents_lived, volunteered, hous
     # @param lived - the amount of time the applicant has lived in california, worked - the amount of time the applicant has worked in california 
     # @param parent_lived - how long the applicants parents lived in california, volunteered - true if the applicant has competed voluteer work false otherwise
     # @return true - if any of the eligibility requirment are met, false - if none of the requirements are met
-    def CA_Residency(lived, worked, parents_lived, volunteered):
+    def ca_residency(lived, worked, parents_lived, volunteered):
         min_years_lived = 2
         min_months_worked = 6
         min_year_parents_lived = 1
@@ -30,8 +37,17 @@ def scholarship_eligibility(age, lived, worked, parents_lived, volunteered, hous
     # determines if the applicant is eligible for the dean consideration 
     # @param household_income - the applicants anual household income 
     # @return true - if the household income is less than 5000
-    def Dean_Consideration(household_income):
+    def dean_consideration(household_income):
         min_household_income = 5000
         return (household_income < min_household_income)
     
-    return (age_req(age) and (CA_Residency(lived, worked, parents_lived, volunteered) or Dean_Consideration(household_income)))
+    if not age_req(age):
+        return Scholarship_Eligibility.INELIGABLE
+    
+    if not CA_Residency(lived, worked, parents_lived, volunteered):
+        if not dean_consideration(house_hold_income):
+            return Scholarship_Eligability.DEAN_CONSIDERATION
+        else:
+            return Scholarship_Eligability.INELIGABLE
+    return Scholarship_Eligability.ELIGABLE
+            
